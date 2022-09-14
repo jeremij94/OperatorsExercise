616bae5dda5423562dac90ea7847efbb265d71a2
jeremij94 <jeremij94@gmail.com>
2022-09-14 13:52:17 -0500

    Operators Exercise First Commit

---
+++ Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace OperatorExercise
+{
+    public class Methods
+    {
+       public static void Exercise1()
+        {
+            // Exercise 1 Math Operators. For divison, create two int variables called a and b. 
+
+
+
+            int a = 17;
+            int b = 4;
+            var quotient = a /= b;
+            var remainder = a %= b;
+
+            Console.WriteLine($" {a} / {b} is {quotient} with a remainder of {remainder}");
+
+            int sum = a += b;
+            Console.WriteLine($"{a} + {b} equals {sum}.");
+
+            int difference = a -= b;
+            Console.WriteLine($"The difference between {a} - {b} equals {difference}.");
+
+            int product = a *= b;
+            Console.WriteLine($"{a} times {b} equals {product}.");
+
+        }
+
+        public static double AreaOfCircle(double radius)
+            {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+            }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Area of a Circle
+
+using OperatorExercise;
+Console.WriteLine ("Please enter the radius of your circle.");
+var radius = double.Parse(Console.ReadLine());
+
+
+
+    Console.WriteLine($"The area of a circle with the radius of {radius} is:");
+
+
+    Console.WriteLine(Methods.AreaOfCircle(radius));
+
+    Console.ReadLine();
+
+
+
+   
+
+
